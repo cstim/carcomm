fdbe93c610ce3131083203fa0128d386252e24b1
Christian Stimming <stimming@tuhh.de>
2010-04-02 22:09:29 +0200

    Add more constraints on the table column data.

--- app/models/slice.rb
+++ app/models/slice.rb
@@ -1,3 +1,8 @@
 class Slice < ActiveRecord::Base
-  validates_presence_of :time, :lat, :lon
+  validates_presence_of :time, :lat, :lon, :avgvel
+  validates_numericality_of :avgvel, :greater_than_or_equal_to => 0, :less_than => 100
+  validates_numericality_of :lon, :startlon,
+    :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180
+  validates_numericality_of :lat, :startlat,
+    :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90
 end
