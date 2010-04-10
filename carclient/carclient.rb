758b78ed2a96a9e6974664ddb88ba08eb5e6a7c4
Christian Stimming <stimming@tuhh.de>
2010-04-10 23:46:21 +0200

    Add qt4-designer created main window. Add the javascript display of the OSM map.

--- carclient/carclient.rb
+++ carclient/carclient.rb
@@ -1,12 +1,40 @@
 #!/usr/bin/ruby
+# http://www.arachnoid.com/ruby/RubyGUIProject/index.html
 
 require 'Qt'
+require 'qtwebkit'
+require 'mainwindow.ui'
+require 'ressources.qrc'
 
-app = Qt::Application.new(ARGV)
+# My class for the main window
+class MyMainWindow < Qt::MainWindow
+  attr_reader :ui
+  attr_writer :ui
 
-button=Qt::PushButton.new("Hello Ruby! &Close me.")
-Qt::Object.connect button, SIGNAL('clicked()'), button, SLOT('close()')
-#button.resize(100,30)
-button.show
+  slots :on_buttonReloadMap_clicked
 
-app.exec
+  def on_buttonReloadMap_clicked
+    puts "We are in on_buttonReloadMap_clicked"
+    @ui.webView.page.mainFrame.evaluateJavaScript('zoomToMuenchen();')
+  end
+end
+
+
+# Create and show our main window
+if $0 == __FILE__
+  app = Qt::Application.new(ARGV)
+
+  mainwindow = MyMainWindow.new
+  mainwindow.ui = Ui_MainWindow.new
+  mainwindow.ui.setupUi(mainwindow)
+
+  mainwindow.show
+
+  # Read the HTML page and write it into the webview widget
+  f = Qt::File.new(':res/streetmap.html')
+  f.open(Qt::IODevice::ReadOnly)
+  x = f.readAll
+  mainwindow.ui.webView.setContent(x)
+
+  app.exec
+end
