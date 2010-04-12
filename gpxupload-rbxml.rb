d4fbbe8e495949f2888ca104edc84d46fc0a7a71
Christian Stimming <stimming@tuhh.de>
2010-04-12 23:19:37 +0200

    Add another gpx upload script that works without gpx gem.

--- gpxupload-rbxml.rb
+++ gpxupload-rbxml.rb
@@ -3,27 +3,43 @@
 # Call this as follows: RUBYLIB=~/.gem/ruby/1.8/gems ./gpxupload.rb
 #
 
-require 'rubygems'
-require 'gpx-0.6/lib/gpx.rb'
-require 'rest_client'
+#require 'rest_client'
+require 'rexml/document'
+require 'time' # this was missing
 
 $Server = 'http://localhost:3000/slices'
 #$Server = 'http://carcomm.cstimming.de/slices'
-$File = ARGV.size > 0 ? ARGV[0] : nil #ENV["HOME"] + "/merkaator-export3.gpx"
+$File = ARGV.size > 0 ? ARGV[0] : ENV["HOME"] + "/merkaator-export3.gpx"
+
+class Trkpt
+  attr_reader :time, :lat, :lon
+  def initialize(elem)
+    if not elem.attribute("lat").nil?
+      @lat = elem.attribute("lat").to_s
+    end
+    if not elem.attribute("lon").nil?
+      @lon = elem.attribute("lon").to_s
+    end
+    if elem.elements["time"]
+      strtime = elem.elements["time"].text
+      @time = Time.parse strtime
+    end
+  end
+end
+
+
 
 def main
-  gpx = GPX::GPXFile.new(:gpx_file => $File)
+  doc = REXML::Document.new File.new($File)
 
-  #puts "Duration: #{gpx.duration}"
   puts "File: #{$File}"
-  puts " Bounds: #{gpx.bounds}"
 
-  gpx.tracks.each do | track |
-    puts " Track Name: #{track.name}"
-    puts "  Num Points: #{track.points.size}"
+  doc.elements.each("gpx/trk/trkseg") do | track |
+    puts " Num Points: #{track.size}"
 
     prev = nil
-    track.points.each do | trkpt |
+    track.elements.each do | elem_trkpt |
+      trkpt = Trkpt.new elem_trkpt
       if prev.nil? or trkpt.time.nil?
         prev = trkpt
       else
