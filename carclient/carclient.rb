1a802c5ad9e1f036dbb93be340a8cb2182432af1
Christian Stimming <stimming@tuhh.de>
2010-04-18 21:28:47 +0200

    Loading the way layer now works - needs retrieving the QWebView content from the same server as the ways.

--- carclient/carclient.rb
+++ carclient/carclient.rb
@@ -25,8 +25,8 @@ class MyMainWindow < Qt::MainWindow
     ui.setupUi(self)
 
     # Our server address
-    # @server = "http://carcomm.cstimming.de"
-    @server = "http://localhost:3000"
+    # @server = "http://carcomm.cstimming.de/"
+    @server = "http://localhost:3000/"
 
     # Read the HTML page and write it into the webview widget
     on_actionResetMap_triggered
@@ -36,11 +36,12 @@ class MyMainWindow < Qt::MainWindow
 
     # Fill the list of available time intervals
     ui.comboBoxInterval.addItem("1 Year", Qt::Variant.new(525600))
-    ui.comboBoxInterval.addItem("5 Minutes", Qt::Variant.new(5))
-    ui.comboBoxInterval.addItem("15 Minutes", Qt::Variant.new(15))
-    ui.comboBoxInterval.addItem("2 Hours", Qt::Variant.new(120))
-    ui.comboBoxInterval.addItem("1 Day", Qt::Variant.new(1440))
+    ui.comboBoxInterval.addItem("6 Weeks", Qt::Variant.new(60480))
     ui.comboBoxInterval.addItem("1 Week", Qt::Variant.new(10080))
+    ui.comboBoxInterval.addItem("1 Day", Qt::Variant.new(1440))
+    ui.comboBoxInterval.addItem("2 Hours", Qt::Variant.new(120))
+    ui.comboBoxInterval.addItem("15 Minutes", Qt::Variant.new(15))
+    ui.comboBoxInterval.addItem("5 Minutes", Qt::Variant.new(5))
   end
 
   def sendDataMaybe
@@ -58,28 +59,33 @@ class MyMainWindow < Qt::MainWindow
   def on_buttonSendData_clicked
     puts "We are in on_buttonSendData_clicked"
     # ADD IMPLEMENTATION
-  end
-
-  def on_buttonReload_clicked
     lat = ui.lineLatitude.text
     lon = ui.lineLongitude.text
     ui.webView.page.mainFrame.evaluateJavaScript("setLatLon(#{lat},#{lon});")
+  end
 
+  def on_buttonReload_clicked
     currentTime = ui.dateTimeEdit.dateTime
     interval = ui.comboBoxInterval.itemData(ui.comboBoxInterval.currentIndex).to_i
     min_time = qtDateTimeToString(currentTime.addSecs(-60 * interval))
     max_time = qtDateTimeToString(currentTime)
-    puts "We are in on_buttonReload_clicked: loadWays(\"#{server}\", \"#{min_time}\", \"#{max_time}\");"
-    ui.webView.page.mainFrame.evaluateJavaScript("loadWays(\"#{server}\", \"#{min_time}\", \"#{max_time}\");")
+    puts "We are in on_buttonReload_clicked: loadWays(\"#{min_time}\", \"#{max_time}\");"
+    ui.webView.page.mainFrame.evaluateJavaScript("loadWays(\"#{min_time}\", \"#{max_time}\");")
   end
 
   def on_actionResetMap_triggered
+    ui.webView.settings.setAttribute(Qt::WebSettings::JavascriptEnabled, true)
+    #ui.webView.settings.setAttribute(Qt::WebSettings::DeveloperExtrasEnabled, true)
+    ui.webView.settings.setAttribute(Qt::WebSettings::JavascriptCanOpenWindows, false)
+    #ui.webView.settings.setAttribute(Qt::WebSettings::JavascriptCanAccessClipboard, true)
+
     # Read the HTML page and write it into the webview widget
     #filename = ':res/test.html'
-    filename = ':res/streetmap.html'
-    f = Qt::File.new(filename)
-    f.open(Qt::IODevice::ReadOnly)
-    ui.webView.setContent(f.readAll)
+    #f = Qt::File.new(filename)
+    #f.open(Qt::IODevice::ReadOnly)
+    #ui.webView.setContent(f.readAll)
+    url = Qt::Url.new(server + "streetmap.html")
+    ui.webView.setUrl(url)
   end
 end
 
