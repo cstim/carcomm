f4ceffd2da58e473abda4d3f4c291a12f1886454
Christian Stimming <stimming@tuhh.de>
2010-06-16 22:52:21 +0200

    Copy the osmdroid code to here and get it to compile.

--- cardroid/gen/de/cstimming/carinfo/R.java
+++ cardroid/gen/de/cstimming/carinfo/R.java
@@ -10,11 +10,40 @@ package de.cstimming.carinfo;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int center=0x7f020000;
+        public static final int direction_arrow=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int marker_default=0x7f020003;
+        public static final int marker_default_focused_base=0x7f020004;
+        public static final int navto_small=0x7f020005;
+        public static final int next=0x7f020006;
+        public static final int person=0x7f020007;
+        public static final int previous=0x7f020008;
+        public static final int zoom_in=0x7f020009;
+        public static final int zoom_out=0x7f02000a;
+    }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
-        public static final int hello=0x7f030001;
+        public static final int about=0x7f040010;
+        public static final int about_message=0x7f040011;
+        public static final int app_name=0x7f040009;
+        public static final int app_name_itemizedoverlay=0x7f04000b;
+        public static final int base=0x7f040004;
+        public static final int cloudmade_small=0x7f040007;
+        public static final int cloudmade_standard=0x7f040008;
+        public static final int cyclemap=0x7f040002;
+        public static final int hello=0x7f04000a;
+        public static final int hills=0x7f040006;
+        public static final int map_mode=0x7f04000d;
+        public static final int mapnik=0x7f040001;
+        public static final int my_location=0x7f04000e;
+        public static final int openareal_sat=0x7f040003;
+        public static final int osmarender=0x7f040000;
+        public static final int samples=0x7f04000f;
+        public static final int topo=0x7f040005;
+        public static final int unknown=0x7f04000c;
     }
 }
