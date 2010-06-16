9a41e81fe0b2f8ae916ab18f3072d7f6be9d87c0
Christian Stimming <stimming@tuhh.de>
2010-06-16 22:28:56 +0200

    Initial android app.

---
+++ cardroid/src/de/cstimming/carinfo/CarinfoActivity.java
@@ -0,0 +1,15 @@
+package de.cstimming.carinfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CarinfoActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
