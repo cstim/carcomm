353579b3befab532eea0c781a450ce77b38401bc
Christian Stimming <stimming@tuhh.de>
2010-06-30 22:01:59 +0200

    Second try: Now add the OSM view widget as a new eclipse-based project. This now runs fine.

---
+++ konphidroid/src/de/cstimming/konphidroid/KonPhiActivity.java
@@ -0,0 +1,13 @@
+package de.cstimming.konphidroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KonPhiActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ konphidroid/src/org/andnav/osm/R.java
@@ -0,0 +1,31 @@
+// Dummy R.java to redirect to a generated one
+
+package org.andnav.osm;
+
+public final class R {
+
+    public static final class drawable {
+        public static final int center = de.cstimming.konphidroid.R.drawable.center;
+        public static final int direction_arrow = de.cstimming.konphidroid.R.drawable.direction_arrow;
+        public static final int marker_default = de.cstimming.konphidroid.R.drawable.marker_default;
+        public static final int marker_default_focused_base = de.cstimming.konphidroid.R.drawable.marker_default_focused_base;
+        public static final int navto_small = de.cstimming.konphidroid.R.drawable.navto_small;
+        public static final int next = de.cstimming.konphidroid.R.drawable.next;
+        public static final int person = de.cstimming.konphidroid.R.drawable.person;
+        public static final int previous = de.cstimming.konphidroid.R.drawable.previous;
+    }
+
+    public static final class string {
+        public static final int osmarender = de.cstimming.konphidroid.R.string.osmarender;
+        public static final int mapnik = de.cstimming.konphidroid.R.string.mapnik;
+        public static final int cyclemap = de.cstimming.konphidroid.R.string.cyclemap;
+        public static final int openareal_sat = de.cstimming.konphidroid.R.string.openareal_sat;
+            public static final int hills = de.cstimming.konphidroid.R.string.hills;
+            public static final int base = de.cstimming.konphidroid.R.string.base;
+            public static final int topo = de.cstimming.konphidroid.R.string.topo;
+        public static final int unknown = de.cstimming.konphidroid.R.string.unknown;
+        public static final int cloudmade_small = de.cstimming.konphidroid.R.string.cloudmade_small;
+        public static final int cloudmade_standard = de.cstimming.konphidroid.R.string.cloudmade_standard;
+    }
+
+}
