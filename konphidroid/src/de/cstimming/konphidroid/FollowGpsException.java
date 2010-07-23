92cc473355fa1f4021002d845226bf04a4e015c6
unknown <stimming@tuhh.de>
2010-07-23 22:26:27 +0200

    Another new try for an android app.

---
+++ konphidroid/src/de/cstimming/konphidroid/FollowGpsException.java
@@ -0,0 +1,15 @@
+package de.cstimming.konphidroid;
+
+import java.io.IOException;
+
+public class FollowGpsException extends IOException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7847957224049800119L;
+
+	public FollowGpsException(final String string) {
+		super(string);
+	}
+
+}
---
+++ konphidroid/src/de/cstimming/konphidroid/KonPhiActivity.java
@@ -0,0 +1,82 @@
+package de.cstimming.konphidroid;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class KonPhiActivity extends Activity implements LocationListener {
+
+	ToggleButton togglebuttonGps;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		togglebuttonGps = (ToggleButton) findViewById(R.id.togglebutton);
+		togglebuttonGps.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// Perform action on clicks
+				if (togglebuttonGps.isChecked()) {
+					Toast.makeText(KonPhiActivity.this, "Checked", Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(KonPhiActivity.this, "Not checked", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+
+		LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(
+				Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				5000, 10,
+				this);
+	}
+
+	@Override
+	public void onLocationChanged(Location loc) {
+		final TextView viewgpsage = (TextView) findViewById(R.id.TextViewGPSAge);
+		final TextView viewLat = (TextView) findViewById(R.id.TextViewLat);
+		final TextView viewLon = (TextView) findViewById(R.id.TextViewLon);
+
+		viewgpsage.setText(loc.getProvider());
+		viewLat.setText(String.valueOf(loc.getLatitude()));
+		viewLon.setText(String.valueOf(loc.getLongitude()));
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		Toast.makeText(KonPhiActivity.this, "Oh: Provider " + provider + " disabled", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		Toast.makeText(KonPhiActivity.this, "Good: Provider " + provider + " enabled", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		switch (status) {
+		case LocationProvider.OUT_OF_SERVICE:
+			Toast.makeText(KonPhiActivity.this, "Oh: Provider " + provider + " out of service", Toast.LENGTH_SHORT).show();
+			togglebuttonGps.setChecked(false);
+			break;
+		case LocationProvider.TEMPORARILY_UNAVAILABLE:
+			Toast.makeText(KonPhiActivity.this, "Oh: Provider " + provider + " tmp. unavailable", Toast.LENGTH_SHORT).show();
+			break;
+		case LocationProvider.AVAILABLE:
+			Toast.makeText(KonPhiActivity.this, "Good: Provider " + provider + " available", Toast.LENGTH_SHORT).show();
+			togglebuttonGps.setChecked(true);
+			break;
+		}
+	}
+}
