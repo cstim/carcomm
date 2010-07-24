096c972b64abf1076df9b3e3bf39ca18b91e2b4a
unknown <stimming@tuhh.de>
2010-07-24 18:42:50 +0200

    Implement sending of the slices to the server. Yay, this really works!

--- konphidroid/src/de/cstimming/konphidroid/KonPhiActivity.java
+++ konphidroid/src/de/cstimming/konphidroid/KonPhiActivity.java
@@ -1,7 +1,21 @@
 package de.cstimming.konphidroid;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -17,6 +31,8 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -24,14 +40,20 @@ import android.widget.ToggleButton;
 public class KonPhiActivity extends Activity implements LocationListener {
 
 	private ToggleButton m_togglebuttonGps;
-	
+	private ToggleButton m_togglebuttonSender;
+
 	private SimpleDateFormat m_dateFormatter;
-	
+	private SimpleDateFormat m_dateFormatSender;
+
 	private Location m_lastLocation;
 	private boolean m_lastLocationValid;
 
 	private long m_senderIntervalSecs;
-	
+	private String m_server = "http://carcomm.cstimming.de/";
+
+	private WebView m_webview;
+	private TextView m_labelSender;
+
 	private static final int DIALOG_GPSWARNING = 1;
 
 	/** Called when the activity is first created. */
@@ -40,15 +62,23 @@ public class KonPhiActivity extends Activity implements LocationListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
+		m_dateFormatSender = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		m_dateFormatSender.setTimeZone(TimeZone.getTimeZone("GMT00"));
 		m_dateFormatter = new SimpleDateFormat("HH:mm:ss");
 		m_senderIntervalSecs = 30;
-		
+		m_togglebuttonGps = (ToggleButton) findViewById(R.id.togglebutton);
+		m_togglebuttonSender = (ToggleButton) findViewById(R.id.togglebuttonSender);
+		m_labelSender = (TextView) findViewById(R.id.TextLabelSender);
+
+		final LinearLayout ell = (LinearLayout) findViewById(R.id.LayoutWebview);
+		m_webview = new WebView(this);
+		ell.addView(m_webview);
+
 		final LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(
 				Context.LOCATION_SERVICE);
 
-		m_togglebuttonGps = (ToggleButton) findViewById(R.id.togglebutton);
 		m_togglebuttonGps.setChecked(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER));
-		
+
 		if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
 			showDialog(DIALOG_GPSWARNING);
 		}
@@ -56,21 +86,26 @@ public class KonPhiActivity extends Activity implements LocationListener {
 		m_togglebuttonGps.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				// Perform action on clicks
-//				if (togglebuttonGps.isChecked()) {
-//					Toast.makeText(KonPhiActivity.this, "Checked", Toast.LENGTH_SHORT).show();
-//				} else {
-//					Toast.makeText(KonPhiActivity.this, "Not checked", Toast.LENGTH_SHORT).show();
-//				}
 				final Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
 				i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
 				startActivity(i);
 				m_togglebuttonGps.setChecked(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER));
 			}
 		});
+		m_togglebuttonSender.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (m_togglebuttonSender.isChecked()) {
+					m_labelSender.setText("Sending Result:");
+				} else {
+					m_labelSender.setText("Paused; Last:");
+				}
+			}
+		});
 
+		long minTime = 1000 * m_senderIntervalSecs; // [milliseconds]
+		float minDistance = 0; //10; // [meters]
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-				5000, 10,
-				this);
+				minTime, minDistance, this);
 	}
 
 	protected Dialog onCreateDialog(int d) {
@@ -79,19 +114,19 @@ public class KonPhiActivity extends Activity implements LocationListener {
 		case DIALOG_GPSWARNING:
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setMessage("The GPS receiver is not active. Do you want to activate it now?")
-			       .setCancelable(false)
-			       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-			           public void onClick(DialogInterface dialog, int id) {
-							final Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-							i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-							startActivity(i);
-			           }
-			       })
-			       .setNegativeButton("No", new DialogInterface.OnClickListener() {
-			           public void onClick(DialogInterface dialog, int id) {
-			                dialog.cancel();
-			           }
-			       });
+			.setCancelable(false)
+			.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					final Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+					i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+					startActivity(i);
+				}
+			})
+			.setNegativeButton("No", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.cancel();
+				}
+			});
 			result = builder.create();
 			break;
 		default:
@@ -99,10 +134,10 @@ public class KonPhiActivity extends Activity implements LocationListener {
 		}
 		return result;
 	}
-	
+
 	@Override
 	public void onLocationChanged(Location loc) {
-		Toast.makeText(KonPhiActivity.this, "Received location from " + loc.getProvider(), Toast.LENGTH_SHORT).show();
+		//Toast.makeText(KonPhiActivity.this, "Received location from " + loc.getProvider(), Toast.LENGTH_SHORT).show();
 
 		final TextView viewgpsage = (TextView) findViewById(R.id.TextViewGPSAge);
 		final TextView viewLat = (TextView) findViewById(R.id.TextViewLat);
@@ -121,21 +156,68 @@ public class KonPhiActivity extends Activity implements LocationListener {
 		if (m_lastLocationValid) {
 			long secdiff = (loc.getTime() - m_lastLocation.getTime()) / 1000;
 			if (secdiff >= m_senderIntervalSecs) {
-				sendPair(m_lastLocation, loc);
+				sendPairNow(m_lastLocation, loc);
 			}
 			secs = String.valueOf(secdiff);
 			dist = String.valueOf(m_lastLocation.distanceTo(loc));
 		}
 		viewDist.setText(dist);
 		viewSecs.setText(secs);
-		
+
 		m_lastLocationValid = true;
 		m_lastLocation = loc;
 	}
 
-	private void sendPair(Location startLoc, Location endLoc) {
-		// TODO Auto-generated method stub
-		
+	private String degToString(double deg) {
+		return String.valueOf(deg);
+	}
+
+	private void sendPairNow(Location startLoc, Location endLoc) {
+		// Only send if the togglebutton is active
+		if (!m_togglebuttonSender.isChecked())
+			return;
+
+		String startLat = degToString(startLoc.getLatitude());
+		String startLon = degToString(startLoc.getLongitude());
+		String endLat = degToString(endLoc.getLatitude());
+		String endLon = degToString(endLoc.getLongitude());
+		String duration = String.valueOf((endLoc.getTime() - startLoc.getTime()) / 1000);
+		String endTime = m_dateFormatSender.format(new Date(endLoc.getTime()));
+		// FIXME: Need to transfer speed and accuracy at both points, and also group selection
+
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(m_server + "slices");
+
+		final TextView resultview = (TextView) findViewById(R.id.TextViewSender);
+		resultview.setText("Sending to " + httppost.getURI().toString());
+		String displaytime = m_dateFormatter.format(new Date(endLoc.getTime())) + ": ";
+		try {
+			// Add your data  
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(6);  
+			nameValuePairs.add(new BasicNameValuePair("slice[startlat]", startLat));  
+			nameValuePairs.add(new BasicNameValuePair("slice[startlon]", startLon));  
+			nameValuePairs.add(new BasicNameValuePair("slice[lat]", endLat));  
+			nameValuePairs.add(new BasicNameValuePair("slice[lon]", endLon));  
+			nameValuePairs.add(new BasicNameValuePair("slice[duration]", duration));  
+			nameValuePairs.add(new BasicNameValuePair("slice[time]", endTime));  
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));  
+
+			//			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			//			httppost.getEntity().writeTo(baos);
+			//			m_webview.loadData(baos.toString(), "text/html", "utf-8");
+
+			// Execute HTTP Post Request  
+			HttpResponse response = httpclient.execute(httppost);
+			//			baos.reset();
+			//			response.getEntity().writeTo(baos);
+			//			m_webview.loadData(baos.toString(), "text/html", "utf-8");
+			StatusLine rstatus = response.getStatusLine();
+			resultview.setText(displaytime + String.valueOf(rstatus.getStatusCode()) + " " + rstatus.getReasonPhrase());  
+		} catch (ClientProtocolException e) {  
+			resultview.setText(displaytime + "Sending failed (Protocol): " + e.getLocalizedMessage());
+		} catch (IOException e) {  
+			resultview.setText(displaytime + "Sending failed (IO): " + e.getLocalizedMessage());
+		}
 	}
 
 	@Override
