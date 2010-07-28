6fa5f173817e0b39e532be3637c8f5c98b1c9954
Christian Stimming <stimming@tuhh.de>
2010-07-28 22:23:35 +0200

    Update database schema accordingly.

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100405150000) do
+ActiveRecord::Schema.define(:version => 20100728201227) do
 
   create_table "slices", :force => true do |t|
     t.datetime "time"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(:version => 20100405150000) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "duration"
+    t.float    "startaccuracy"
+    t.float    "endaccuracy"
+    t.integer  "instanceid"
+    t.integer  "categoryid"
   end
 
 end
