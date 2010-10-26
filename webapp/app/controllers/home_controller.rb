cstim/carcomm

commit e3eb81957d7ad7117f284f2f15cbbf9e4ef7e944

Implement NMEA file upload to the webserver.

webapp/app/controllers/home_controller.rb
@@ -1,9 +1,18 @@
 require 'rexml/document'
 require 'time'
+require 'date'
 
+# Represents one point of a GPX or NMEA track
 class Trkpt
   attr_reader :time, :lat, :lon
-  def initialize(elem)
+
+  def initialize(_time, _lat, _lon)
+    @time = _time
+    @lat = _lat
+    @lon = _lon
+  end
+
+  def Trkpt.fromXml(elem)
     if not elem.attribute("lat").nil?
       @lat = elem.attribute("lat").to_s
     end
@@ -17,14 +26,55 @@ class Trkpt
   end
 end
 
+# Convert a NMEA coordinate string to a floating point number
+def dmmm2dec(degrees, sw)
+  deg = (degrees/100.0).floor #decimal degrees
+  frac = ((degrees/100.0)-deg)/0.6 #decimal fraction
+  ret = deg+frac #positive return value
+  if ((sw=="S") or (sw=="W")):
+      ret=ret*(-1) #flip sign if south or west
+  end
+  return ret
+end
 
+# Post a slice into our database
+def postSlice(trkpt, prev)
+  duration = prev.time.nil? ? 0 : trkpt.time - prev.time
+  if duration < 95
+    slice = Slice.new(:time => trkpt.time,
+                      :lat => trkpt.lat,
+                      :lon => trkpt.lon,
+                      :startlat => prev.lat,
+                      :startlon => prev.lon,
+                      :duration => duration)
+    slice.save
+    return true
+  else
+    return false
+  end
+end
+
+
+# The controller class
 class HomeController < ApplicationController
+  attr_reader :minSliceDuration
+
+  def initialize
+    @minSliceDuration = 30
+    super
+  end
+
   def index
   end
 
   def gpxUpload
     resulttext = storeGpx(params[:upload])
     render :text => "File has been uploaded successfully: #{resulttext}"
+  end
+
+  def nmeaUpload
+    resulttext = storeNMEA(params[:upload])
+    render :text => "File has been uploaded successfully: #{resulttext}, with duration #{minSliceDuration} seconds each."
   end
 
   protected
@@ -37,22 +87,15 @@ class HomeController < ApplicationController
     doc.elements.each("gpx/trk/trkseg") do | track |
       prev = nil
       track.elements.each do | elem_trkpt |
-        trkpt = Trkpt.new elem_trkpt
+        trkpt = Trkpt.fromXml elem_trkpt
         if prev.nil? or trkpt.time.nil?
           prev = trkpt
         else
           duration = prev.time.nil? ? 0 : trkpt.time - prev.time
-          # Skip slices smaller than 9 seconds
-          if duration > 29
+          # Skip slices smaller than 30 seconds
+          if duration > minSliceDuration
             # Ignore everything above 95 seconds altogether
-            if duration < 95
-              slice = Slice.new(:time => trkpt.time,
-                                :lat => trkpt.lat,
-                                :lon => trkpt.lon,
-                                :startlat => prev.lat,
-                                :startlon => prev.lon,
-                                :duration => duration)
-              slice.save
+            if postSlice(trkpt, prev)
               newslices = 1 + newslices
             end
             prev = trkpt
@@ -60,6 +103,50 @@ class HomeController < ApplicationController
         end
       end # track.points.each
     end # gpx.tracks.each
+    "Added #{newslices} new slices from #{originalname}"
+  end
+
+  protected
+  def storeNMEA(file)
+    originalname = file.original_filename
+
+    date = nil
+    prev = nil
+    newslices = 0
+
+    while (line = file.gets) do
+      if line =~ /\$GPRMC/
+        tokens = line.split(",")
+        date = Date.strptime(tokens[9], '%d%m%y')
+      end
+      if line =~ /\$GPGGA/
+        tokens = line.split(",")
+        time = Time.parse tokens[1]
+        if tokens[6] != '0' and not date.nil?
+          datetime = Time.utc(date.year, date.month, date.day,
+                              time.hour, time.min, time.sec, time.usec)
+          lat = dmmm2dec((tokens[2]).to_f,tokens[3])
+          lon = dmmm2dec((tokens[4]).to_f,tokens[5])
+          trkpt = Trkpt.new(datetime, lat, lon)
+
+          if prev.nil? or trkpt.time.nil?
+            prev = trkpt
+          else
+            duration = prev.time.nil? ? 0 : trkpt.time - prev.time
+            # Skip slices smaller than the minimum duration
+            if duration >= minSliceDuration
+              # Ignore everything above 95 seconds altogether
+              if postSlice(trkpt, prev)
+                newslices = 1 + newslices
+              end
+              prev = trkpt
+            end
+          end # end else prev.nil? or trkpt.time.nil?
+
+        end # end tokens[6] != '0' and not date.nil?
+      end # end line =~ /\$GPGGA/
+    end # end while (line = file.gets) do
+
     "Added #{newslices} new slices from #{originalname}"
   end
 end

gpxupload-rbxml.rb
@@ -5,15 +5,23 @@
 
 #require 'rest_client'
 require 'rexml/document'
-require 'time' # this was missing
+require 'time'
+require 'date'
 
 $Server = 'http://localhost:3000/slices'
 #$Server = 'http://carcomm.cstimming.de/slices'
-$File = ARGV.size > 0 ? ARGV[0] : ENV["HOME"] + "/merkaator-export3.gpx"
+$File = ARGV.size > 0 ? ARGV[0] : nil
 
 class Trkpt
   attr_reader :time, :lat, :lon
-  def initialize(elem)
+
+  def initialize(_time, _lat, _lon)
+    @time = _time
+    @lat = _lat
+    @lon = _lon
+  end
+
+  def Trkpt.fromXml(elem)
     if not elem.attribute("lat").nil?
       @lat = elem.attribute("lat").to_s
     end
@@ -27,27 +35,21 @@ class Trkpt
   end
 end
 
+# Convert a NMEA coordinate string to a floating point number
+def dmmm2dec(degrees, sw)
+  deg = (degrees/100.0).floor #decimal degrees
+  frac = ((degrees/100.0)-deg)/0.6 #decimal fraction
+  ret = deg+frac #positive return value
+  if ((sw=="S") or (sw=="W")):
+      ret=ret*(-1) #flip sign if south or west
+  end
+  return ret
+end
 
-
-def main
-  doc = REXML::Document.new File.new($File)
-
-  puts "File: #{$File}"
-
-  doc.elements.each("gpx/trk/trkseg") do | track |
-    puts " Num Points: #{track.size}"
-
-    prev = nil
-    track.elements.each do | elem_trkpt |
-      trkpt = Trkpt.new elem_trkpt
-      if prev.nil? or trkpt.time.nil?
-        prev = trkpt
-      else
-        duration = prev.time.nil? ? 0 : trkpt.time - prev.time
-        # Ignore slices smaller than 5 seconds
-        if duration > 9
-          if duration < 95
-            puts "Time: #{trkpt.time} Lat: #{trkpt.lat} Lon: #{trkpt.lon} Duration: #{duration}s Startlat: #{prev.lat} Startlon: #{prev.lon}"
+def sendWay(trkpt, prev)
+  duration = prev.time.nil? ? 0 : trkpt.time - prev.time
+  if duration < 95
+    puts "Time: #{trkpt.time} Lat: #{trkpt.lat} Lon: #{trkpt.lon} Duration: #{duration}s Startlat: #{prev.lat} Startlon: #{prev.lon}"
 
 #             RestClient.post $Server, {
 #               "slice[lat]" => trkpt.lat,
@@ -56,11 +58,80 @@ def main
 #               "slice[duration]" => duration,
 #               "slice[startlat]" => prev.lat,
 #               "slice[startlon]" => prev.lon }
-          end
+  end
+end
+
+
+def main
+  if $File =~ /gpx$/
+    # GPX file
+    doc = REXML::Document.new File.new($File)
+
+    puts "File: #{$File}"
+
+    doc.elements.each("gpx/trk/trkseg") do | track |
+      puts " Num Points: #{track.size}"
+
+      prev = nil
+      track.elements.each do | elem_trkpt |
+        trkpt = Trkpt.fromXml elem_trkpt
+        if prev.nil? or trkpt.time.nil?
           prev = trkpt
+        else
+          duration = prev.time.nil? ? 0 : trkpt.time - prev.time
+          # Ignore slices smaller than 9 seconds
+          if duration > 9
+            sendWay(trkpt, prev)
+            prev = trkpt
+          end
         end
       end
     end
+  else
+    # NMEA file
+    file = File.new($File)
+
+    puts "File: #{$File}"
+
+    date = nil
+    prev = nil
+
+    while (line = file.gets) do
+      if line =~ /\$GPRMC/
+        tokens = line.split(",")
+        #time = Time.parse tokens[1]
+        date = Date.strptime(tokens[9], '%d%m%y')
+        #datetime = Time.utc(date.year, date.month, date.day,
+        #                    time.hour, time.min, time.sec, time.usec)
+        #puts "RMC datetime=#{datetime}"
+      end
+      if line =~ /\$GPGGA/
+        tokens = line.split(",")
+        time = Time.parse tokens[1]
+        if tokens[6] != '0' and not date.nil?
+          datetime = Time.utc(date.year, date.month, date.day,
+                              time.hour, time.min, time.sec, time.usec)
+          lat = dmmm2dec((tokens[2]).to_f,tokens[3]) #[2] is lat in deg+minutes, [3] is {N|S|W|E}
+          lon = dmmm2dec((tokens[4]).to_f,tokens[5]) #[4] is long in deg+minutes, [5] is {N|S|W|E}
+          #puts "Time=#{datetime} Lat=#{lat}, Lon=#{lon}"
+          trkpt = Trkpt.new(datetime, lat, lon)
+          #sleep 1
+
+          if prev.nil? or trkpt.time.nil?
+            prev = trkpt
+          else
+            duration = prev.time.nil? ? 0 : trkpt.time - prev.time
+            # Ignore slices smaller than 9 seconds
+            if duration > 2
+              sendWay(trkpt, prev)
+              prev = trkpt
+            end
+          end
+
+        end
+      end
+    end
+
   end
 
 end

webapp/app/views/home/index.html.erb
@@ -212,7 +212,15 @@ File: </label>
 <%= file_field_tag 'upload' %>
 and <%= submit_tag "Upload GPX" %>
 <% end %>
-<p>(GPX upload will store way slices in a 30-second raster, not finer
+
+<% form_tag('home/nmeaUpload', :multipart => true ) do %>
+<label for="upload">You can upload a NMEA file with GPS logs. Select
+File: </label>
+<%= file_field_tag 'upload' %>
+and <%= submit_tag "Upload NMEA" %>
+<% end %>
+
+<p>(File upload will store way slices in a 30-second raster, not finer
 than that for now.)</p>
 
 <h2>Displaying the ways on a map</h2>

commit 395b6747f0459fb6ab697e2e2bd45edc043db0db

Fix GPX file uploading which was broken since e3eb81957d7ad7117f284f2f15cbbf9e4ef7e944

webapp/app/controllers/home_controller.rb
@@ -19,7 +19,7 @@ class Trkpt
     @categoryid = 0
   end
 
-  def Trkpt.fromXml(elem)
+  def fromXml(elem)
     @avgvel = nil
     @headingdeg = nil
     @accuracy = 0
@@ -75,7 +75,7 @@ def postSlice(trkpt, prev)
                       :startlon => prev.lon,
                       :duration => duration,
                       :avgvel => trkpt.avgvel,
-                      :headingdev => trkpt.headingdeg,
+                      :headingdeg => trkpt.headingdeg,
                       :startaccuracy => prev.accuracy,
                       :endaccuracy => trkpt.accuracy,
                       :instanceid => trkpt.instanceid,
@@ -120,7 +120,9 @@ class HomeController < ApplicationController
     doc.elements.each("gpx/trk/trkseg") do | track |
       prev = nil
       track.elements.each do | elem_trkpt |
-        trkpt = Trkpt.fromXml elem_trkpt
+        trkpt = Trkpt.new(nil, nil, nil)
+        trkpt.fromXml elem_trkpt
+        #logger.info("#{Time.now} Trying trkpt, got #{trkpt} from #{elem_trkpt}")
         if prev.nil? or trkpt.time.nil?
           prev = trkpt
         else