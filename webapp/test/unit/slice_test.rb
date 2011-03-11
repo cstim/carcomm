681b1cc4bc0e014235b9f75a641fd412b486a2a0
Christian Stimming <stimming@tuhh.de>
2011-03-11 21:36:33 +0100

    Some rudimentary unittesting

--- webapp/test/unit/slice_test.rb
+++ webapp/test/unit/slice_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
+require 'geo'
 
 class SliceTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+#  test "the truth" do
+#    assert true
+#  end
+  test "geo haversine_dist" do
+    assert Geo.haversine_dist(2,4,3,5)# == 314283.255073684 
   end
 end
