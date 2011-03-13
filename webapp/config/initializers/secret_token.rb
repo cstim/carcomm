c6f0544b54f00aec0ee05b3c02df008065fc60ca
Christian Stimming <stimming@tuhh.de>
2011-03-13 22:17:38 +0100

    Upgrade webapp to rails 3.

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webapp::Application.config.secret_token = 'd5b5abf9ee44dad6d5acd22ba0ae1a1a8a37325c5b5130ff7508d6bcbb2fefabfe875ed1f5d71604529cf3c89ef0df4b48576b0d59ddea3e5861bcffbe816bc4'
