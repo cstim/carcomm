1a6d0647493c033b880fdfca2a11a29a3d079ae4
Christian Stimming <stimming@tuhh.de>
2011-03-16 22:26:13 +0100

    Implement Stausignal for moving out of stau as well

--- webapp/lib/algo.rb
+++ webapp/lib/algo.rb
@@ -1,48 +1,71 @@
 require 'time'
 
 class Algo
-  def self.predict1(tdata)
+  def self.time_to_still(tdata)
     if tdata.empty?
       return 0
     end
 
-	# Current velocity in [km/h]
+    # Current velocity in [km/h]
     v_current = tdata.last[1]
-	# The current time - probably zero.
-	t_current = tdata.last[0]
+    # The current time - probably zero.
+    t_current = tdata.last[0]
 
-	# Find the last index where the block is true
-	t_30_index = tdata.rindex{|i| t_current - i[0] >= 30}
+    # Find the last index where the block is true
+    t_30_index = tdata.rindex{|i| t_current - i[0] >= 30}
 
-	# This is the measurement approx. 30 seconds ago, and its velocity
-	t_30 = tdata[t_30_index][0]
-	v_30 = tdata[t_30_index][1]
-	#puts "t_30_index=#{t_30_index}, v_30=#{v_30}"
+    # This is the measurement approx. 30 seconds ago, and its velocity
+    t_30 = tdata[t_30_index][0]
+    v_30 = tdata[t_30_index][1]
+    #puts "t_30_index=#{t_30_index}, v_30=#{v_30}"
 
-	t_diff = t_current - t_30
-	v_diff = v_current - v_30
+    # The max and min velocity in our data
+    v_minmax = tdata.minmax_by{|i| i[1]}
+    v_min = v_minmax[0][1]
+    v_max = v_minmax[1][1]
 
-	m = v_diff.to_f / t_diff.to_f
-	puts "t_diff=#{t_diff} v_diff=#{v_diff} m=#{m}"
+    is_in_jam = v_max < 60 && v_min < 30
+
+    t_diff = t_current - t_30
+    v_diff = v_current - v_30
+
+    m = v_diff.to_f / t_diff.to_f
+    puts "t_diff=#{t_diff} v_diff=#{v_diff} m=#{m} in_jam=#{is_in_jam} v_min=#{v_min} v_max=#{v_max}"
 
     if v_current >= 60
+      # Free flowing traffic, except if we're braking
       if m < -0.9
-		bis_stillstand = v_current / 1.1
-		return bis_stillstand.round.to_s + " Sek bis Stillstand"
-	  else
-		return "0 Kein Stau"
-	  end
+        # Currently braking
+        bis_stillstand = v_current / 1.1
+        return bis_stillstand.round.to_s + " Sek bis Stillstand"
+      else
+        # No jam
+        return "0 Kein Stau"
+      end
     elsif v_current >= 30
-      if m < -0.9
-		bis_stillstand = v_current / 1.7
-		return bis_stillstand.round.to_s + " Sek bis Stillstand"
-	  else
-		return "0 Kein Stau"
-	  end
+      if is_in_jam
+        # Medium-fast traffic; moving out of jam
+        if m > 0.5
+          until_jam_finish = 120 + v_current
+          return until_jam_finish.round.to_s + " Sek bis freie Fahrt"
+        else
+          return "-1 Stau"
+        end
+      else
+        # Medium-fast traffic; moving into jam
+        if m < -0.9
+          until_standing = v_current / 1.7
+          return until_standing.round.to_s + " Sek bis Stillstand"
+        else
+          return "0 Kein Stau"
+        end
+      end
     else
+      # Slow traffic; in jam
       return "-1 Stau"
     end
   end
+
 end
 
 def print_tdata(tdata)
@@ -55,16 +78,19 @@ def test_main1
   tdata = [	[-30, 90], [-20, 12], [-10, 11], [0, 80] ]
   #print_tdata(tdata)
 
-  puts "Test Result1 = " + Algo.predict1(tdata).to_s
+  puts "Test Result1 = " + Algo.time_to_still(tdata).to_s
 
   tdata = [	[-30, 120], [-20, 12], [-10, 11], [0, 61] ]
-  puts "Test Result2 = " + Algo.predict1(tdata).to_s
+  puts "Test Result2 = " + Algo.time_to_still(tdata).to_s
 
   tdata = [	[-30, 70], [-20, 90], [-10, 11], [0, 35] ]
-  puts "Test Result3 = " + Algo.predict1(tdata).to_s
+  puts "Test Result3 = " + Algo.time_to_still(tdata).to_s
 
   tdata = [	[-30, 46], [-20, 12], [-10, 11], [0, 25] ]
-  puts "Test Result4 = " + Algo.predict1(tdata).to_s
+  puts "Test Result4 = " + Algo.time_to_still(tdata).to_s
+
+  tdata = [	[-30, 12], [-20, 12], [-10, 11], [0, 35] ]
+  puts "Test Result4 = " + Algo.time_to_still(tdata).to_s
 
 end
 
